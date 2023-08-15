2e3a4c4c314578a432c4a89c88a984529cd373c0
Luke Harrison <wlharr04@gmail.com>
2023-08-15 14:20:22 -0400

    increased modularity

--- piglatin.py
+++ piglatin.py
@@ -1,18 +1,3 @@
-import input_analysis
-import translator
-import text_formatter
-import file_handler
-
-def main():
-    while True:
-        input_string = input("Enter phrase to translate into Pig Latin or q to quit:\n")
-        if input_string == "q":
-            print("Oodbyegay orfay ownay!")
-            break
-        text_map = input_analysis.analyze(input_string)
-        translated_words = translator.translate(text_map)
-        output_string = text_formatter.make_output_string(translated_words)
-        print("\nIn Pig Latin:", output_string, sep = "\n", end = "\n\n")
-        file_handler.save_file(output_string)
+import main
         
 main()
