1e014f21c9280885c964780fb3ba732d706e2590
Luke Harrison <wlharr04@gmail.com>
2023-12-02 01:14:41 -0500

    added wordwrap functionality

--- text_formatter.py
+++ text_formatter.py
@@ -1,8 +1,16 @@
+import textwrap
+
 def make_output_string(translated_words):
-    output_string = ""
+    input_string = ""
+    output_string = ''
 
     for word in translated_words:
-        output_string += word + " "
+        input_string += word + " "
+
+    lines = textwrap.wrap(input_string)
+
+    for line in lines:
+        output_string += f'{line}\n'
 
     output_string = output_string.strip()
 
